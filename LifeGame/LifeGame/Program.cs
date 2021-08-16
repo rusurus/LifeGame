6873f62c58ef134bea7da9e37f419cc18e9d5141
rusurus <urusov74@gmail.com>
2021-08-16 22:57:38 +0300

    create project and create parametrs for universe

---
+++ LifeGame/LifeGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LifeGame
+{
+    class Program
+    {
+        static void Main() {
+            
+            //create array
+            Console.WriteLine("Hello! Enter universe size. From 1 to 255:");
+            byte arraySize = Convert.ToByte(Console.ReadLine());
+            byte[,] firstGeneration = new byte[arraySize,arraySize];
+            
+            //calculating cells count
+            int cellsCount = Convert.ToInt32(Math.Pow(arraySize, 2));
+            
+            Console.WriteLine("Universe was created. Size is {0}x{1}. Cells count is {2}", arraySize, arraySize, cellsCount);
+            
+            //calculating live cells
+            Console.WriteLine("Now, enter life cells rate. From 1 to 99 ");
+            byte lifeRate = Convert.ToByte(Console.ReadLine());
+            int lifeCellsCount = cellsCount * lifeRate / 100;
+            Console.WriteLine("Life cells count is " + lifeCellsCount);
+            
+            
+
+        }
+    }
+}
